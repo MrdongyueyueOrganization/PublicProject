0d16548eea03ec2430f2e19f09f35010ce617ee4
董知樾 <dongzhiyue@dongzhiyuedeMacBook-Pro.local>
2017-03-23 22:14:26 +0800

    Initial Commit

---
+++ PublicProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PublicProject
+//
+//  Created by 董知樾 on 2017/3/23.
+//  Copyright © 2017年 董知樾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
